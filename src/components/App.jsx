d79e661d1f8c9d67d4b29062a9addbf2d7d97763
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-09 23:53:07 +0100

    searchbar component is added and styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,31 @@
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+import css from './App.module.css';
+import Searchbar from './Searchbar';
+
+export default class App extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  handleQuery = query => {
+    this.setState({ searchQuery: query });
+  };
+
+  render() {
+    return (
+      <div className={css.App}>
+        <Searchbar onSubmit={this.handleQuery} />
+
+        <ToastContainer
+          autoClose={3000}
+          hideProgressBar={false}
+          closeOnClick
+          pauseOnHover
+        />
+      </div>
+    );
+  }
+}
