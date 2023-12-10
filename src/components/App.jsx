401ac4b80e852f6c107a070df957a2490ba3695a
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-10 22:10:22 +0100

    button component to load more picture is added and stylized, corresponding logic is added to App either

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import css from './App.module.css';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
+import Button from './Button';
 import api from '../services/api';
 
 export default class App extends Component {
@@ -12,16 +13,28 @@ export default class App extends Component {
     searchQuery: '',
     page: 1,
     picturesSet: [],
+    searchMatches: 0,
+    totalHits: 0,
   };
 
   componentDidUpdate(prevProps, prevState) {
     const { searchQuery, page } = this.state;
     if (prevState.searchQuery !== searchQuery) {
-      const { searchQuery } = this.state;
       try {
-        api(searchQuery, page).then(pictures =>
-          this.setState({ picturesSet: pictures.hits })
-        );
+        api(searchQuery, page).then(pictures => {
+          this.setState(
+            prevState => ({
+              picturesSet: pictures.hits,
+              searchMatches: prevState.searchMatches + pictures.hits.length,
+              totalHits: pictures.totalHits,
+            }),
+            () => {
+              console.log(
+                `we have loaded ${this.state.searchMatches} out of ${this.state.totalHits} totally found`
+              );
+            }
+          );
+        });
       } catch (error) {
         if (error.message === 'Network Error') {
           console.log(
@@ -35,15 +48,53 @@ export default class App extends Component {
   }
 
   handleQuery = query => {
-    this.setState({ searchQuery: query });
+    this.setState({
+      searchQuery: query,
+      page: 1,
+      picturesSet: [],
+      searchMatches: 0,
+      totalHits: 0,
+    });
+  };
+
+  loadMorePictures = () => {
+    const { searchQuery, page } = this.state;
+    try {
+      api(searchQuery, page + 1).then(pictures => {
+        this.setState(
+          prevState => ({
+            picturesSet: pictures.hits,
+            searchMatches: prevState.searchMatches + pictures.hits.length,
+            page: prevState.page + 1,
+          }),
+          () => {
+            console.log(
+              `we have loaded ${this.state.searchMatches} out of ${this.state.totalHits} totally found`
+            );
+          }
+        );
+      });
+    } catch (error) {
+      if (error.message === 'Network Error') {
+        console.log(
+          'Internet connection is lost. Please try again as soon as your connection is restored'
+        );
+      } else {
+        console.log('An error occurred: ', error.message);
+      }
+    }
   };
 
   render() {
-    const { picturesSet } = this.state;
+    const { picturesSet, searchMatches, totalHits } = this.state;
     return (
       <div className={css.App}>
         <Searchbar onSubmit={this.handleQuery} />
-        <ImageGallery pictures={picturesSet} />
+
+        {picturesSet.length > 0 && <ImageGallery pictures={picturesSet} />}
+        {searchMatches < totalHits && (
+          <Button onClick={this.loadMorePictures} />
+        )}
         <ToastContainer
           autoClose={3000}
           hideProgressBar={false}
