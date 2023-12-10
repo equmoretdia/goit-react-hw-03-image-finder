a43b3ff62b19c1ea3bc7bdcc127593cc48c980f9
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-10 17:51:53 +0100

    api file is added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,27 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import css from './App.module.css';
 import Searchbar from './Searchbar';
+import api from '../services/api';
 
 export default class App extends Component {
   state = {
     searchQuery: '',
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { searchQuery } = this.state;
+    try {
+      api(searchQuery, 1);
+    } catch (error) {
+      if (error.message === 'Network Error') {
+        console.log(
+          'Internet connection is lost. Please try again as soon as your connection is restored'
+        );
+      } else {
+        console.log('An error occurred: ', error.message);
+      }
+    }
+  }
 
   handleQuery = query => {
     this.setState({ searchQuery: query });
---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '40252258-b27561441daedadb4fc814a5c';
+const imageType = 'photo';
+const orientation = 'horizontal';
+const picturesPerPage = 12;
+
+export default async function fetchPictures(query, page) {
+  const response = await axios(
+    `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=${imageType}&orientation=${orientation}&per_page=${picturesPerPage}`
+  );
+  if (response.status < 200 || response.status >= 300) {
+    throw new Error(response.status);
+  }
+  const pictures = response.data;
+  return pictures;
+}
